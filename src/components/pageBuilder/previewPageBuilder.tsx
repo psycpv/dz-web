15303c435d4979a53531e81d06eef7bfb1e8607f
David Torres <jdtcto@gmail.com>
2023-04-04 13:12:58 -0500

    NWEB-217: remove exit button

--- src/components/pageBuilder/previewPageBuilder.tsx
+++ src/components/pageBuilder/previewPageBuilder.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import {FC} from 'react'
 
 import {PageBuilder} from '@/components/pageBuilder'
@@ -9,17 +8,7 @@ interface PreviewPageBuilderProps {}
 
 export const PreviewPageBuilder: FC<PreviewPageBuilderProps> = () => {
   const data = usePreview(null, homePage)
-  return (
-    <>
-      <PageBuilder rows={homeMapper(data)} />
-      <Link
-        className="text-white fixed bottom-0 right-0 bg-blue-500 p-6 font-bold"
-        href="/api/sanity/exit-preview"
-      >
-        Exit Preview
-      </Link>
-    </>
-  )
+  return <PageBuilder rows={homeMapper(data)} />
 }
 
 export default PreviewPageBuilder
