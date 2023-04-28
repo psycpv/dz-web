0aa64dd0c43e1a167b6258a0fa215b6f5e164ce7
Fabio Tovar <fabiostovarr@gmail.com>
2023-04-28 16:19:43 -0500

    Remove footer non used fields

--- src/sanity/schema/documents/footer.ts
+++ src/sanity/schema/documents/footer.ts
@@ -1,46 +1,18 @@
 import {BlockElementIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
-
-import location from './location'
-import page from './page'
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'footer',
   title: 'Footer',
   type: 'document',
   icon: BlockElementIcon,
-  preview: {
-    select: {pageTitle: 'page.title', docTitle: 'title', defaultNav: 'default'},
-    prepare: ({defaultNav, pageTitle, docTitle}) => ({
-      title: defaultNav ? `Default - ${docTitle}` : pageTitle,
-    }),
-  },
+  preview: {prepare: () => ({title: 'Footer'})},
   fields: [
     defineField({
       name: 'title',
-      description: 'This field is the footer title',
       title: 'Title',
       type: 'string',
       validation: (rule) => rule.required(),
-    }),
-    defineField({name: 'default', title: 'Default footer?', type: 'boolean', initialValue: false}),
-    defineField({
-      name: 'page',
-      title: 'Page',
-      type: 'reference',
-      hidden: (params) => params.parent.default === true,
-      to: [{type: page.name}],
-    }),
-    defineField({
-      name: 'locations',
-      title: 'Locations',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          name: 'gallery',
-          type: location.name,
-        }),
-      ],
     }),
   ],
 })
