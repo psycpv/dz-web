68e09a4231af0565ac93c4cd69ab37062cc6dd34
David Torres <jdtcto@gmail.com>
2023-05-08 14:48:08 -0500

    NWEB-391: remove unused package and double config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const {theme} = require('@sanity/demo/tailwind')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   content: [
     './src/pages/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
