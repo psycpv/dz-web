dd21b45ba6d6dcadf0db3e7e0642576faae7a539
Doug Tabuchi <doug@dougtabuchi.com>
2023-05-23 11:37:11 -0400

    Change reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,9 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // 'github' for GitHub Actions CI to generate annotations, plus a concise 'dot'
+  // default 'list' when running locally
+  reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
