2bdad8cd96bd60b4b9e0bea5d9bd4c32d0181e8a
Fabio Tovar <fabiostovarr@gmail.com>
2023-06-02 20:51:04 -0500

    Fix articles mapper

--- src/components/containers/home/mapper.ts
+++ src/components/containers/home/mapper.ts
@@ -60,9 +60,7 @@ export const mapFeaturedContentSplit = (data: any) => {
 
 export const mapArticlesGrid = (data = []) => {
   return data?.map((item) => {
-    // TODO fix typo in the sanity studio
-    const {title, subtitle, images = []} = item ?? {}
-    const [mainImage] = images ?? []
+    const {title, subtitle, mainImage} = item ?? {}
     const {asset, alt} = mainImage ?? {}
     const imgSrc = asset ? builder.image(asset).url() : ''
     return {
