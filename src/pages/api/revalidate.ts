7fcdfac91a20bd250e9185f6b032d967c9f9b5a3
adam krawesky <adam@davidzwirner.com>
2023-06-29 14:42:53 -0400

    log revalidation error

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,45 +10,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.query.secret !== process.env.ISR_TOKEN) {
     return res.status(401).json({message: 'Invalid token'})
   }
+  const slug = req.body?.slug?.current
+  const type = req.body?._type
+  let basePath
 
+  // externalNews is content hosted on an external site so we cannot update it
+  if (req.body?.type === 'externalNews') {
+    return res.json({revalidated: false})
+  }
+  // TODO Enable ISR for these when https://zwirner.atlassian.net/browse/NWEB-553 is fixed
+  if (['exhibitionPage', 'article'].includes(type)) {
+    return res.json({revalidated: false})
+  }
+
+  // Reconstruct the NextJS page route to revalidate based on the respective Sanity schema
+  // e.g. /artists/tomma-abts, /utopia-editions
+  if (type === 'home') {
+    basePath = '/'
+  } else if (slug) {
+    basePath = TypesToPathsMap[type] ? `/${TypesToPathsMap[type]}` : undefined
+  } else {
+    basePath = `/${camelToDash(type)}`
+  }
+
+  if (!type || !basePath) {
+    return res.json({revalidated: false})
+  }
   try {
-    const slug = req.body?.slug?.current
-    const type = req.body?._type
-    let basePath
-
-    // externalNews is content hosted on an external site so we cannot update it
-    if (req.body?.type === 'externalNews') {
-      return res.json({revalidated: false})
-    }
-    // TODO Enable ISR for these when https://zwirner.atlassian.net/browse/NWEB-553 is fixed
-    if (['exhibitionPage', 'article'].includes(type)) {
-      return res.json({revalidated: false})
-    }
-
-    // Reconstruct the NextJS page route to revalidate based on the respective Sanity schema
-    // e.g. /artists/tomma-abts, /utopia-editions
-    if (type === 'home') {
-      basePath = '/'
-    } else if (slug) {
-      basePath = TypesToPathsMap[type] ? `/${TypesToPathsMap[type]}` : undefined
-    } else {
-      basePath = `/${camelToDash(type)}`
-    }
-
-    if (!type || !basePath) {
-      return res.json({revalidated: false})
-    }
     await res.revalidate(slug ? `${basePath}${slug}` : basePath)
     return res.json({revalidated: true})
   } catch (err) {
-    // TODO report an error to surface internally to notify us the revalidation failed
-    // We cannot return a res.status(500) because it will cause Sanity to retry the request
-    // for 30 minutes and will not allow other requests to complete until the failing request
-    // is either resolved or times out https://www.sanity.io/docs/webhooks#33326b412b2d
-
-
+    console.error('[revalidate]', err);
+    // TODO check err for invalid path, return res.status(401)
     return res.json({revalidated: false})
-
-
   }
 }
