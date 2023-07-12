9f722edb948e97a1ada52c4812d09e6313ed6197
adam krawesky <adam.krawesky@gmail.com>
2023-07-12 13:06:08 -0400

    fix missing title and slug in query

--- src/sanity/queries/artworkByArtist.queries.ts
+++ src/sanity/queries/artworkByArtist.queries.ts
@@ -1,16 +1,11 @@
 import {groq} from 'next-sanity'
 
-import {artworkFields} from '@/sanity/queries/artwork.queries'
-
-// TODO actual query by artist id / slug / something
-export const artworkByArtist = groq`
-*[_type == "artwork"] | order(date desc, _updatedAt desc) {
-  ${artworkFields}
-}`
-
 export const artworksDataByArtistSlug = groq`
 *[_type == "artistPage" && slug.current == $slug]{
+  title,
+  slug { current },
   surveySubpage {
+    title,
     items[]->{...}
   }
 }`
