b438c28ab52be0fc112de2863c7309b9ecb32422
adam krawesky <adam.krawesky@gmail.com>
2023-07-13 17:38:05 -0400

    add container title for artist exhibitions

--- src/components/containers/pages/artists/exhibitions/index.tsx
+++ src/components/containers/pages/artists/exhibitions/index.tsx
@@ -1,7 +1,8 @@
-import {DzColumn, DzComplexGrid} from '@zwirner/design-system'
+import {DzColumn, DzComplexGrid, TITLE_SIZES} from '@zwirner/design-system'
 
 import {FullWidthFlexCol} from '@/components/containers/layout/FullWidthFlexCol'
 import ArtistsPageLayout from '@/components/containers/layout/pages/artistsPageLayout'
+import {ContainerTitle} from '@/components/containers/title/ContainerTitle'
 
 import {mapCardsGrid} from './mapper'
 
@@ -13,10 +14,12 @@ const ArtistExhibitionsPageContainer = ({data}: PageContainerProps) => {
   const parentPath = data?.artistSlug
   const parentPageTitle = data?.artistFullName
   const complexGridCards = mapCardsGrid(data?.exhibitions)
+  const title = `EXHIBITIONS: ${parentPageTitle}`
 
   return (
     <ArtistsPageLayout parentPageName={parentPageTitle} parentPath={parentPath}>
       <DzColumn span={12}>
+        <ContainerTitle title={title} titleSize={TITLE_SIZES.XL} />
         <FullWidthFlexCol>
           <DzComplexGrid cards={complexGridCards} />
         </FullWidthFlexCol>
