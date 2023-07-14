3726d6907e0af260e6855ba175b363350d224e99
adam krawesky <adam.krawesky@gmail.com>
2023-07-14 14:16:25 -0400

    use common copy for page title

--- src/components/containers/pages/artists/exhibitions/index.tsx
+++ src/components/containers/pages/artists/exhibitions/index.tsx
@@ -1,6 +1,7 @@
 import {DzColumn, DzComplexGrid, DzInterstitial, TITLE_SIZES} from '@zwirner/design-system'
 import {Fragment} from 'react'
 
+import {EXHIBITIONS} from '@/common/constants/commonCopies'
 import {FullWidthFlexCol} from '@/components/containers/layout/FullWidthFlexCol'
 import ArtistsPageLayout from '@/components/containers/layout/pages/artistsPageLayout'
 import {ContainerTitle} from '@/components/containers/title/ContainerTitle'
@@ -15,7 +16,7 @@ const ArtistExhibitionsPageContainer = ({data}: PageContainerProps) => {
   const parentPath = data?.slug
   const parentPageTitle = data?.artistFullName
   const complexGridCards = mapCardsGrid(data?.exhibitions)
-  const title = `EXHIBITIONS: ${parentPageTitle}`
+  const title = `${EXHIBITIONS.toUpperCase()}: ${parentPageTitle}`
   const interstitialData = interstitialMap(data?.interstitial)
 
   return (
