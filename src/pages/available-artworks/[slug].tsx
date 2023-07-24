7b3fae0c6e08e2e30ae4242d4b3580ba95c8a578
adam krawesky <adam.krawesky@gmail.com>
2023-07-24 14:22:50 -0400

    add example zoom page

---
+++ src/pages/available-artworks/[slug].tsx
@@ -0,0 +1,12 @@
+import {DzImageZoom} from '@zwirner/design-system'
+
+const AvailableArtworkPage = () => {
+  return (
+    <div>
+      Available artwork page
+      <DzImageZoom />
+    </div>
+  )
+}
+
+export default AvailableArtworkPage
