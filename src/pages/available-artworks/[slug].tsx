daf85d204dc9aa6f4abd80fdf7fb4dbc513c47a9
adam krawesky <adam.krawesky@gmail.com>
2023-07-25 10:15:28 -0400

    wip

--- src/pages/available-artworks/[slug].tsx
+++ src/pages/available-artworks/[slug].tsx
@@ -1,11 +1,38 @@
-import {DzImageZoom} from '@zwirner/design-system'
+import {DzColumn} from '@zwirner/design-system'
+import {FullWidthFlexCol} from '@/components/containers/layout/FullWidthFlexCol'
+
+/*
+import {DzImageZoomModal} from '@zwirner/design-system'
+
+import {FullWidthFlexCol} from '@/components/containers/layout/FullWidthFlexCol'
+*/
+import DzArtworkDetail from '../../../../design-system/src/molecules/DzArtworkDetail/DzArtworkDetail'
 
 const AvailableArtworkPage = () => {
+  /*
   return (
-    <div>
-      Available artwork page
-      <DzImageZoom />
-    </div>
+    <DzColumn span={12}>
+      <FullWidthFlexCol>
+        <DzImageZoomModal
+          imgUrl="http://localhost:3000/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fjuzvn5an%2Fartists%2F19209716a83e9ce1adfe63caa13b2e7b64d96c6d-2160x1228.webp&w=2048&q=75"
+          onClose={() => {}}
+          isOpen={true}
+        />
+      </FullWidthFlexCol>
+    </DzColumn>
+  )
+   */
+  return (
+    <DzColumn span={12}>
+      <FullWidthFlexCol>
+        <DzArtworkDetail
+          artistName="John Smith"
+          artworkTitle="Painting Number One"
+          description="This is a description with a really long piece of text"
+          mediaItems={[]}
+        />
+      </FullWidthFlexCol>
+    </DzColumn>
   )
 }
 
