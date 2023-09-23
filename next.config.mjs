355c1f219dda7995e104c3ff362d659bec1c381a
Doug Tabuchi <39024+tabuchid@users.noreply.github.com>
2023-09-23 08:58:10 -0400

    Add sentry (#312)

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import {withSentryConfig} from '@sentry/nextjs'
 import analyzer from '@next/bundle-analyzer'
 const {env} = await import('./src/env.mjs')
 
@@ -15,4 +16,34 @@ const withBundleAnalyzer = analyzer({
   enabled: env.ANALYZE,
 })
 
-export default withBundleAnalyzer(config)
+export default withSentryConfig(
+  withBundleAnalyzer(config),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: 'zwirner',
+    project: 'web',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+)
