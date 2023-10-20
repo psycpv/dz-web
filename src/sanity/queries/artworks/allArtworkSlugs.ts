9c09d27c494134dd11ae746563a8eb840a43dddd
Fabio Tovar <fabiostovarr@gmail.com>
2023-10-20 12:15:41 -0500

    Limit artworks to be built to 100 pages (#390)

--- src/sanity/queries/artworks/allArtworkSlugs.ts
+++ src/sanity/queries/artworks/allArtworkSlugs.ts
@@ -2,6 +2,7 @@ import {groq} from 'next-sanity'
 import {z} from 'zod'
 
 export const allArtworkSlugs = groq`
-*[_type == "artwork" && defined(slug)].slug.current`
+*[_type == "artwork" && defined(slug) && defined(dateSelection.year)][0...100].slug.current
+`
 
 export const AllArtworkSlugsSchema = z.array(z.string())
