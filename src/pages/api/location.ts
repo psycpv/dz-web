7fb69a7d31340965159563699d7cefac4e5ba656
Doug Tabuchi <39024+tabuchid@users.noreply.github.com>
2023-11-07 14:18:24 -0500

    Decode city location (#443)

--- src/pages/api/location.ts
+++ src/pages/api/location.ts
@@ -3,6 +3,11 @@ import {NextApiRequest, NextApiResponse} from 'next'
 export default function handler(request: NextApiRequest, response: NextApiResponse) {
   const {headers} = request
 
+  const cityHeader = headers['x-vercel-ip-city']
+  const city = Array.isArray(cityHeader) ? cityHeader[0] : cityHeader
+
+  const decodedCity = city ? decodeURIComponent(city) : undefined
+
   response.status(200).json({
     ip: headers['x-real-ip'],
     timezone: headers['x-vercel-ip-timezone'],
@@ -10,6 +15,6 @@ export default function handler(request: NextApiRequest, response: NextApiRespon
     longitude: headers['x-vercel-ip-longitude'],
     country: headers['x-vercel-ip-country'],
     region: headers['x-vercel-ip-country-region'],
-    city: headers['x-vercel-ip-city'],
+    city: decodedCity,
   })
 }
