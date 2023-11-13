a5a82e0ef7ff05d04d043294880990e293f8f98f
Andriy Trypilka <54888768+tripolkaandrey@users.noreply.github.com>
2023-11-13 16:15:00 +0100

    NWEB-131: Integrate Lighthouse into CI (#448)
    * Integrate Lighthouse into CI
    
    * Run Lighthouse on staging
    
    * Switch to tracking deployment_status
    
    * Remove the lighthouse step from CI
    
    * Remove redundant
    
    * Inline `sparse-checkout-cone-mode`
    
    * Leverage `autorun` for updating status checks
    
    * Set job name
    
    * Assume Lighthouse runs only in CI
    
    * Remove 404 links from lighthouse config
    
    * Add the recommended assertions
    
    * Change assertions to `no-pwa`
    
    * Clean up
    
    * Add suffix for GitHub status checks
    
    * Increase number of runs to 3
    
    * Adjust  URLs for Lighthouse analysis
    
    * Add concurrency group

---
+++ .github/workflows/lighthouserc.js
@@ -0,0 +1,32 @@
+const APP_URL = process.env.ENVIRONMENT_URL
+
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        `${APP_URL}/`,
+        `${APP_URL}/artists`,
+        `${APP_URL}/exhibitions`,
+        `${APP_URL}/exhibitions/2023/ethan-exhibition-test-primary-subtitle`,
+        `${APP_URL}/available-artworks`,
+        `${APP_URL}/artists/tomma-abts`,
+        `${APP_URL}/artists/tomma-abts/survey`,
+        `${APP_URL}/artists/tomma-abts/guide`,
+        `${APP_URL}/artists/tomma-abts/press`,
+        `${APP_URL}/artworks/tomma-abts-oeje-7a744`,
+        `${APP_URL}/news/2018/tomma-abts-in-berlin`,
+      ],
+      numberOfRuns: 3,
+      settings: {
+        preset: 'desktop'
+      }
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa'
+    },
+    upload: {
+      target: 'temporary-public-storage',
+      githubStatusContextSuffix: '/desktop'
+    }
+  }
+}
