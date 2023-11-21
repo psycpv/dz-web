de7ac72e563d3125674a7162e57ea15dec047107
adam <adam@davidzwirner.com>
2023-11-21 12:51:47 -0500

    fix import linting (#510)

--- src/components/pageBuilder/DzCard/cardMapper.tsx
+++ src/components/pageBuilder/DzCard/cardMapper.tsx
@@ -12,10 +12,10 @@ import {dzMediaMapper} from '@/common/utilsMappers/image.mapper'
 import {safeText} from '@/common/utilsMappers/safe'
 // TODO: extract utils to a general mapper for availability
 import {parseAvailability} from '@/components/containers/home/utils'
-import {ArtworkFramedSchema} from '@/sanity/queries/components/content/artworkContent'
 import {createInquireModalArtworkProps} from '@/components/hooks/useOpenInquiryDispatch'
 import {exhibitionCommonMapper} from '@/components/pageBuilder/utils/common'
 import {cardContentArticle} from '@/components/pageBuilder/utils/commonMappers'
+import {ArtworkFramedSchema} from '@/sanity/queries/components/content/artworkContent'
 import {BookContentType} from '@/sanity/queries/components/content/bookContent'
 import {BookVariation, DzCardExtendedProps} from '@/sanity/types'
 
