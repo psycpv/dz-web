5d8ec0ffa58fcdd514e2cb3c4ff25299663dc3a5
PsychicPower <psycpv@gmail.com>
2023-11-28 07:54:44 -0500

    fix: check GA4 ecommerce type (#534)

--- src/common/utilsMappers/gtm.mapper.ts
+++ src/common/utilsMappers/gtm.mapper.ts
@@ -19,35 +19,35 @@ export const mapEcommerceData: MapEcommerceData = (artwork, extra) => {
     items: [
       {
         affiliation: 'DZW',
-        artwork_ecommerce: artwork?.product?.variants[0].store?.inventory?.isAvailable
+        artwork_ecommerce: artwork.product?.variants[0].store?.inventory?.isAvailable
           ? 'yes'
           : 'no',
-        artwork_frame: artwork?.framed,
-        artwork_price: artwork?.price?.toString(),
-        artwork_size: artwork?.dimensions ? artwork?.dimensions[0]?.children[0]?.text : '',
-        artwork_status: artwork?.product?.variants[0].store?.inventory?.isAvailable
+        artwork_frame: artwork.framed,
+        artwork_price: artwork.price?.toString() || '',
+        artwork_size: artwork.dimensions ? artwork.dimensions[0]?.children[0]?.text || '' : '',
+        artwork_status: artwork.product?.variants[0].store?.inventory?.isAvailable
           ? 'available'
           : 'sold',
-        item_brand: artwork?.artists?.at(0)?.fullName,
+        item_brand: artwork.artists?.at(0)?.fullName || '',
         item_category: title,
         item_category2: section,
-        item_category3: artwork?.artworkType,
-        item_category4: artwork?.medium,
-        item_category5: artwork?._createdAt,
-        item_id: artwork?.inventoryId,
-        item_name: artwork?.title,
-        item_variant: artwork?.dateSelection?.year,
+        item_category3: artwork.artworkType,
+        item_category4: artwork.medium || '',
+        item_category5: artwork._createdAt,
+        item_id: artwork.inventoryId || '',
+        item_name: artwork.title,
+        item_variant: artwork.dateSelection.year,
       },
     ],
   }
 
   if (extra?.item_list_id) ecommerce.item_list_id = hash
   if (extra?.item_list_name) ecommerce.item_list_name = title
-  if (extra?.currency) ecommerce.currency = artwork?.currency
+  if (extra?.currency) ecommerce.currency = artwork.currency || ''
   if (extra?.items?.item_list_id && ecommerce.items[0]) ecommerce.items[0].item_list_id = hash
   if (extra?.items?.item_list_name && ecommerce.items[0]) ecommerce.items[0].item_list_name = title
   if (extra?.items?.price && ecommerce.items[0])
-    ecommerce.items[0].price = artwork?.product?.variants[0].store?.price
+    ecommerce.items[0].price = artwork.product?.variants[0].store?.price || 0
 
   return ecommerce
 }
@@ -73,7 +73,7 @@ declare global {
   type DataLayerEcommerceProps = {
     count_checkout_payment_step_completions?: number
     coupon?: string
-    currency?: 'EUR' | 'USD' | 'GBP' | 'HKD' | null | undefined
+    currency?: 'EUR' | 'USD' | 'GBP' | 'HKD' | ''
     facets?: string
     item_list_id?: string
     item_list_name?: string
@@ -81,16 +81,16 @@ declare global {
       affiliation: string
       artwork_ecommerce: string
       artwork_frame: string
-      artwork_price: string | null | undefined
+      artwork_price: string
       artwork_size: string
       artwork_status: string
       item_brand: string
       item_category: string
       item_category2: string
       item_category3: string
-      item_category4: string | null
+      item_category4: string
       item_category5: string
-      item_id: string | null
+      item_id: string
       item_list_id?: string
       item_list_name?: string
       item_name: string
